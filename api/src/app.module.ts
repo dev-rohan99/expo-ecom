db66af978a035564a1effe35ca12780383c35019
dev-rohan99 <rohanmostofa599@gmail.com>
2026-01-23 11:05:15 +0600

    feat(dev): added order service logic!

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -9,6 +9,7 @@ import { PrismaModule } from 'prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 import { CartModule } from './cart/cart.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { CartModule } from './cart/cart.module';
     ProductsModule,
     CategoriesModule,
     CartModule,
+    OrdersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ api/src/orders/orders.service.ts
@@ -0,0 +1,73 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+
+@Injectable()
+export class OrdersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createOrderFromCart(userId: string) {
+    const cart = await this.prisma.cart.findUnique({
+      where: { userId },
+      include: { cartItems: { include: { product: true } } },
+    });
+
+    if (!cart || cart.cartItems.length === 0) {
+      throw new BadRequestException('Cart id empty!');
+    }
+
+    let totalAmount = 0;
+
+    for (const item of cart.cartItems) {
+      const product = item.product;
+
+      if (!product || !product.isActive) {
+        throw new NotFoundException(`Product unavailable: ${item.productId}`);
+      }
+
+      if (item.quantity < product.stock) {
+        throw new BadRequestException(
+          `Insufficient stock for ${product.title}`,
+        );
+      }
+
+      totalAmount += item.quantity * product.price;
+    }
+
+    return this.prisma.$transaction(async (tx) => {
+      const order = await tx.order.create({
+        data: {
+          userId,
+          totalAmount,
+        },
+      });
+
+      for (const item of cart.cartItems) {
+        await tx.orderItem.create({
+          data: {
+            orderId: order.id,
+            productId: item.productId,
+            quantity: item.quantity,
+            price: item.product.price,
+          },
+        });
+
+        await tx.product.update({
+          where: { id: item.productId },
+          data: {
+            stock: {
+              decrement: item.quantity,
+            },
+          },
+        });
+
+        await tx.cartItem.deleteMany({ where: { cartId: item.cartId } });
+
+        return order;
+      }
+    });
+  }
+}
